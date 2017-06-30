0bcfd373f7549bb6c6163ebc2c34c963ea13a637
= <antonpaly@ya.ru>
2017-06-30 15:56:01 +0500

    changed base name for test

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ docks = './docs/'
 files_folder = os.getcwd() + docks
 
 gitlab_url = 'https://projects.rtk-sdo.ru'
-db_path = docks+'localbase.db'
+db_path = docks+'testbase.db'
+#db_path = docks+'localbase.db'
 issues_csv_file = docks+'issues.csv'
 xls_file = docks+'issues.xlsx'
 email = 'antonpaly@ya.ru'
